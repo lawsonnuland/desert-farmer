b81a0e7645086249ec10504a9d2f6c519d0f3946
Lawson Nuland <lawson.nuland@gmail.com>
2022-08-03 15:08:36 -0400

    Settled on style for game, started scripting

--- index.js
+++ index.js
@@ -1,9 +1,38 @@
 const board = document.querySelector('.board');
 
-function generateBoard() {
-    board.textContent = "game pieces go here";
+function generateBoard(tiles) {
+    let greenTiles=0;
+    while(board.firstChild) {
+        board.removeChild(board.firstChild);
+    }
+
+    board.style.gridTemplateColumns = `repeat(${Math.floor(Math.sqrt(tiles + 4))}, 1fr)`;
+
+    for(let i=0; i<tiles; i++) {
+        let div = document.createElement('div');
+        div.classList.add('tile');
+        board.appendChild(div);
+    }
+
+    let allTiles = document.querySelectorAll('.tile');
+    allTiles.forEach(tile => {
+        tile.addEventListener('click', () => {
+            if (tile.classList.contains('wet')) {
+                tile.style.background = "green";
+                greenTiles++;
+                if(greenTiles == tiles) {
+                    //alert("All green!");
+                }
+            } else {
+                tile.style.background = "teal";
+                tile.classList.add('wet');
+            }
+        })
+    });
 }
 
 window.addEventListener('load', () => {
-    generateBoard();
+    generateBoard(4);
 })
+
+//TODO Add zai, funds, changing land over time
